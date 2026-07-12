010e1aa8e534a5a2f99a7d81a42df94e17c01c5b
jaylordlapena817-star <jaylordlapena817@gmail.com>
2026-07-12 22:00:45 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { 
-  getAuth 
-} from "firebase/auth";
-
-import {
-  getFirestore
-} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
   apiKey: "AIzaSyDTJFiY42avRGMqtKrZB7gPhWL-ASTRh3w",
   authDomain: "mybot-d79df.firebaseapp.com",
-  databaseURL: "https://mybot-d79df-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mybot-d79df",
   storageBucket: "mybot-d79df.firebasestorage.app",
   messagingSenderId: "215753443154",
-  appId: "1:215753443154:web:af883fb18af1d499a15c8b",
+  appId: "1:215753443154:web:af883fb18af1d499a15c8b"
 };
 
 
@@ -23,5 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
